3243b2e4d8cc3fc137904dac4763cb589f24c2ec
maya-nachiappan <maya.n102304@gmail.com>
2024-09-25 01:33:05 -0700

    initialized variables, used a string to store the binary value of a number by constantly dividing it by 2 and prepending the remainder to the string while the number is greater than 0

--- binary representation of integers/main.cpp
+++ binary representation of integers/main.cpp
@@ -6,9 +6,38 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int convertBinary(int num)
+{
+if (num==0)
+ {
+    cout << "the binary representation is 0" << endl;
+    return 0;
+  }
+    string binary="";
+    
+    while (number>0)
+    {
+        binary=to_string(num % 2)+binary;
+        num=num/2;
+    }
+    cout <<"the binary representation is "<< binary<<endl;
+}
+
+int main()
+{
+    int num;
+    cout << "enter a positive number: " << endl;
+    cin >> num;
+    
+    // checks if the number is actually positive
+    if (num<0)
+    {
+        cout <<"you did not enter a positive number. please try again." <<endl;
+        return 1;
+    }
+    cout << covertBinary(num) <<endl;
     return 0;
 }
