f1cc796ae3b41c09c76395dbd3a4140c0b3b3a7d
maya-nachiappan <maya.n102304@gmail.com>
2024-10-14 01:59:18 -0700

    added gitignore file and comments

--- binary representation of integers/main.cpp
+++ binary representation of integers/main.cpp
@@ -9,7 +9,7 @@
 #include <string>
 using namespace std;
 
-int convertBinary(int num)
+int convertBinary(int num) // creates function to convert number to binary code
 {
     if (num==0)
     {
@@ -20,11 +20,11 @@ int convertBinary(int num)
     
     while (num > 0)
     {
-        // divides num by 2 and adds remainder to string
+        // divides num by 2 and adds remainder to string using concatenation
         binary=to_string(num % 2)+binary;
         num=num/2; // keeps dividing num by 2
     }
-    cout <<"the binary representation is "<< binary<<endl;
+    cout <<"the binary representation is "<< binary<<endl; // prints the binary value
     return 0;
 }
 
@@ -32,7 +32,7 @@ int main()
 {
     int num;
     cout << "enter a positive number: " << endl;
-    cin >> num;
+    cin >> num; // takes num from user
     
     // checks if the number is actually positive
     if (num<0)
@@ -40,5 +40,5 @@ int main()
         cout <<"you did not enter a positive number. please try again." <<endl;
         return 1;
     }
-    cout << convertBinary(num) <<endl;
+    cout << convertBinary(num) <<endl; // converts and prints the binary value using convertBinary function
 }
